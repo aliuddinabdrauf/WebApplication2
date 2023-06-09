115be8d759a8a2cb226d3a07481771333c5b74a4
aliuddin <ali1404udin@gmail.com>
2023-06-09 22:31:50 +0800

    Add project files.

---
+++ WebApplication2/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Dto;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public bool Test() { return true; }
+
+        [HttpGet]
+        public int ReturnBack(int id) { return id; }
+
+        [HttpPost]
+        public PersonDto AddPerson(PersonDto person)
+        {
+            return person;
+        }
+    }
+}
---
+++ WebApplication2/Dto/PersonDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace WebApplication2.Dto
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Username { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+    }
+}
